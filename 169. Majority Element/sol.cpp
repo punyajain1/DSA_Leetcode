0259d01e4f56c7023f1915790338aecd4c4d535f
punyajain1 <punyajain50@gmail.com>
2025-08-17 16:47:22 +0530

    169. Majority Element

---
+++ 169. Majority Element/sol.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        int n = nums.size()/2;
+        for(auto it:mp){
+            if(it.second > n){
+                return it.first;
+            }
+        }
+        return -1;
+        
+    }
+};
