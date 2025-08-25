c797b763e933e682ba009033ad5990282a3563c4
punyajain1 <punyajain50@gmail.com>
2025-08-25 19:12:07 +0530

    Agressive Cow BS

---
+++ Agressive cow Binary Search/sol.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+bool dis(vector<int>& v, int mid,int cow){
+    int last=v[0];
+    int count=1;
+    for(int i=1;i<v.size();i++){
+        if(v[i]-last >= mid){
+            count++;
+            last=v[i];
+        }
+        if(count>=cow) return true;
+    }
+    return false;
+}
+
+int main(){
+    int n, ans=-1 , cows=2;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    int l=0,h=v.size();
+    while(l<h){
+        int mid=l+(h-l)/2;
+        if(dis(v,mid,cows)==true){
+            ans=mid;
+            l=mid+1;
+        }else{
+            h=mid-1;
+        }
+    }
+    cout<<ans<<endl;
+}
