59416e2466405fd6a93f16926a83bae12504a195
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-08-30 19:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (98.78%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -11,14 +11,15 @@ public:
         int count=t.size();
         int l=0,r=0,d=INT_MAX,head=0;
         while(r < s.size()){
-            if(need[s[r++]]-->0) count--;
+            if(need[s[r]]-->0) count--;
+            r++;
+            
             while(count == 0){
                 if(r-l < d){
                     head=l;
                     d=r-head;
-                }if(need[s[l++]]++ == 0){
-                    count++;
-                }
+                }if(need[s[l]]++ == 0) count++;
+                l++;
             }
         }
         return d == INT_MAX ? "" : s.substr(head, d);
