08eee62327f88bf933fa01efab82583ffdbe4f38
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-08-30 19:29:12 +0530

    Time: 3 ms (82.22%) | Memory: 11.4 MB (84.27%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(s.size()==0|| t.size()==0){
+            return "";
+        }
+        vector<int> need(128,0);
+        for(auto it : t){
+            need[it]++;
+        }
+        int count=t.size();
+        int l=0,r=0,d=INT_MAX,head=0;
+        while(r < s.size()){
+            if(need[s[r++]]-->0) count--;
+            while(count == 0){
+                if(r-l < d){
+                    head=l;
+                    d=r-head;
+                }if(need[s[l++]]++ == 0){
+                    count++;
+                }
+            }
+        }
+        return d == INT_MAX ? "" : s.substr(head, d);
+    }
+};
