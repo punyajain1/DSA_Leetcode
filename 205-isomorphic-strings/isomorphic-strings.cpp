e7babd2583d54dbfaf97f131a4bc14a54662603e
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-08-30 19:53:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (81.14%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,7 +4,7 @@ public:
         if(s.size()!=t.size()) return false;
         vector<int> m1(200,0),m2(200,0);
         for(int i=0;i<s.length();i++){
-            if(m1[s[i]] != m2[t[i]]) {
+            if(m1[s[i]] != m2[t[i]]) { // here we are using char as the index for the m1 and m2 as the can store all the ined of all ascii char , char is coverted into its ascii int so that we can use it 
                 return false;
             }
             m1[s[i]] = i+1;
