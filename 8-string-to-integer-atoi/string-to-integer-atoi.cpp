18306a4a6bd639fa458a8e09f09c40744e3614bd
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-08-31 15:38:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.02%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -4,14 +4,12 @@ public:
         long long ans=0;
         int sign=1,i=0,n=s.size();
         while(isspace(s[i]) && i<n) i++;
-        if(s[i]=='-')
-        {
+        if(s[i]=='-'){
             sign=-1;
             i++;
-        }else if(s[i]=='+')i++;
+        }else if(s[i]=='+') i++;
         while(i<n && isdigit(s[i])){
-            int d=s[i]-'0';
-            ans=ans*10+d;
+            ans=ans*10+(s[i]-'0');
             if(ans>INT_MAX && sign==-1) return INT_MIN;
             else if(ans>INT_MAX && sign==1) return INT_MAX;
             i++;
