603adbca8a5633902e139c0f97b6da862c97d964
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-08-31 15:58:59 +0530

    Time: 123 ms (38.36%) | Memory: 26.2 MB (26.24%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int beautySum(string s) {
         int ans=0;
-        int n=s.size();
+        int n=s.length();
         for(int i=0;i<n;i++){
             unordered_map<char, int>mp;
             for(int j=i;j<n;j++){
                 mp[s[j]]++;
-                int maxf=0,minf=INT_MAX;
+                int maxf=0;
+                int minf=INT_MAX;
                 for(auto it: mp){
                     maxf=max(maxf,it.second);
                     minf=min(minf,it.second);
