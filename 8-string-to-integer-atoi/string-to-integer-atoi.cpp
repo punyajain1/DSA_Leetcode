0bcb51e99eb9b6c5b3338c68fcd496910ae91e1f
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-08-31 15:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (82.00%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    int myAtoi(string s) 
-    {
-        int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
-            i++;
+    int myAtoi(string s) {
+        long long ans=0;
+        int sign=1,i=0,n=s.size();
+        while(isspace(s[i]) && i<n) i++;
         if(s[i]=='-')
         {
             sign=-1;
             i++;
-        }
-        else if(s[i]=='+')
+        }else if(s[i]=='+')i++;
+        while(i<n && isdigit(s[i])){
+            int d=s[i]-'0';
+            ans=ans*10+d;
+            if(ans>INT_MAX && sign==-1) return INT_MIN;
+            else if(ans>INT_MAX && sign==1) return INT_MAX;
             i++;
-        while(i<s.length())
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
-                i++;
-            }
-            else
-                return ans*sign;
         }
-        return (ans*sign);
+        return (int)ans*sign;
     }
 };
