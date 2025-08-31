4f5a9108c70e63c85b4bb01883415dfb37aa07b4
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-08-31 16:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (85.01%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        string ans;
+        int i=s.length()-1;
+        while (i >= 0) {
+            while (i>=0 && isspace(s[i])) i--;
+            if (i < 0) break;
+            int j = i;
+            while (i >= 0 && !isspace(s[i])) i--;
+            if (!ans.empty()) ans += " ";
+            ans += s.substr(i + 1, j - i);
+        }
+        
+        return ans;
+    }
+};
