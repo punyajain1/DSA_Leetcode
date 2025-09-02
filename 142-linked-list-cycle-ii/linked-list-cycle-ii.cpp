d999fae2b510c82d699c8e7b0b105e6268db0839
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-09-02 15:59:16 +0530

    Time: 5 ms (83.74%) | Memory: 11.2 MB (83.11%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -12,7 +12,7 @@ public:
         ListNode *slow = head;
         ListNode *fast = head;
 
-        while(fast!=NULL && fast->next!=NULL){
+        while(fast!=nullptr && fast->next!=nullptr){
             fast=fast->next->next;
             slow=slow->next;
             if(slow==fast){
