95f24989326b3a48b537d90f1d580b0996bdce08
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-09-03 18:50:59 +0530

    Time: 0 ms (100.00%) | Memory: 77 MB (74.01%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -6,15 +6,18 @@ public:
         int carry=0;
         while(l1!=nullptr || l2!=nullptr || carry!=0){
             int sum =carry;
-            if(l1!=nullptr) sum+=l1->val;
-            if(l2!=nullptr) sum+=l2->val;
-
+            if(l1){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2){
+                sum+=l2->val;
+                l2=l2->next;
+            }
             carry=sum/10;
             ListNode* n = new ListNode(sum%10);
             t->next=n;
             t=t->next;
-            if (l1 != nullptr) l1=l1->next;
-            if (l2 != nullptr) l2=l2->next;
         }
         return d->next;
     }
