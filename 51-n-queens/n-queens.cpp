adeb506c5b8b1af92b780565942bbb1493a8ecae
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-09-19 21:41:55 +0530

    Time: 1 ms (81.17%) | Memory: 10.2 MB (53.44%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-vector<vector<string>>ans;
-vector<int>car;
-int n;
-bool issafe(int r,int c){
-    for(int pr=0;pr<r;pr++){
-        int pc=car[pr];
-        if(pc==c)
-        return false;
-        if(abs(pc-c)==abs(pr-r))
-        return false;
-
-    }
-    return true;
-}
-
-void backtrack(int r){
-    if(r==n){
-             vector<string> board(n, string(n, '.'));
-            for (int i = 0; i < n;i++)
-             board[i][car[i]] = 'Q';
+    void s(int col,vector<vector<string>> &ans, vector<string> &board , vector<int> &leftR,vector<int> &lb,vector<int> &ub,int n){
+        if(col==n){
             ans.push_back(board);
             return;
-    }
-    for(int c=0;c<n;c++){
-        if(issafe(r,c)){
-            car[r]=c;
-            backtrack(r+1);
+        }
+        for(int row=0;row<n;row++){
+            if(leftR[row]==0 && lb[row+col]==0 && ub[n-1+col-row]==0){
+                board[row][col]='Q';
+                leftR[row]=1;
+                lb[row+col]=1;
+                ub[n-1+col-row]=1;
+                s(col+1,ans,board,leftR,lb,ub,n);
+                board[row][col]='.';
+                leftR[row]=0;
+                lb[row+col]=0;
+                ub[n-1+col-row]=0;
+            }
         }
     }
-}
-    vector<vector<string>> solveNQueens(int N) {
-        n=N;
-        ans.clear();
-        car.assign(n,-1);
-        backtrack(0);
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+        vector<int> leftR(n,0), lb(2*n -1),ub(2*n -1);
+        s(0,ans,board,leftR,lb,ub,n);
         return ans;
     }
 };
