3763cc1aa35b3ab248b70a7f0b74ea9e4cb58e82
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-09-23 16:46:42 +0530

    Time: 1 ms (33.96%) | Memory: 14 MB (35.70%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int a = 0, b = 1, c = 0;
+        for (auto &num: nums) {
+            a ^= num;
+        }
+        while (not (a & b)) {
+            b <<= 1;
+        }
+        for (auto &num: nums){
+		    if (num & b) c ^= num;
+        }
+	    return {c, a ^ c};
+    }
+};
