80b8b9f8260b9e6128845918ddad93baae6e0ecc
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-10-07 18:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (92.39%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -6,13 +6,18 @@ public:
         for(int r=0;r<s.size();r++){
             count[s[r]-'A']++;
             mx=max(mx,count[s[r]-'A']);
-
             if((r-l+1-mx) > k){
                 count[s[l]-'A']--;
                 l++;
             }
-            res=max(res , r-l+1);
+            res=max(res,r-l+1);
         }
         return res;
     }
 };
+
+
+//here we are checking if the lenght of current substring after removing the 
+            //same chare(char which is present at r) is more then k if yes then we cant
+            // replace the char so we increase l by one and remove the acc of char at l 
+            // before increasing by 1
