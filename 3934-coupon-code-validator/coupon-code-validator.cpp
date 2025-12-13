e6e4956387a88aac94177442e022117f3330c00e
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-12-13 12:38:39 +0530

    Time: 0 ms (100.00%) | Memory: 68.9 MB (70.82%) - LeetSync

---
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string &s) {
+        if(s.empty()) return false;
+        for(char ch : s) {
+            if(!isalnum(ch) && ch != '_') return false;
+        }
+        return true;
+    }
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        vector<string> ans;
+        vector<pair<string, string>> arr;
+        for(int i=0; i<code.size(); i++) {
+            if(isValid(code[i]) && (businessLine[i] == "electronics" || businessLine[i] == "grocery" || businessLine[i] == "pharmacy" || businessLine[i] == "restaurant") && isActive[i]){
+                arr.push_back({businessLine[i], code[i]});
+            }
+        }
+        sort(arr.begin(), arr.end());
+        for(int i=0; i<arr.size(); i++) {
+            ans.push_back(arr[i].second);
+        }
+        return ans;
+    }
+};
