773e9cd56be5b559dbb895998b8a450b7b7580d9
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2025-12-21 18:58:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (62.92%) - LeetSync

---
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+        vector<bool> sorted(n - 1, false);
+        int ans = 0;
+
+        for (int col = 0; col < m; col++) {
+            bool bad = false;
+
+            for (int row = 0; row + 1 < n; row++) {
+                if (!sorted[row] && strs[row][col] > strs[row + 1][col]) {
+                    bad = true;
+                    break;
+                }
+            }
+
+            if (bad) {
+                ans++;
+                continue;
+            }
+
+            for (int row = 0; row + 1 < n; row++) {
+                if (strs[row][col] < strs[row + 1][col]) {
+                    sorted[row] = true;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
