36669770578a3f8737792621e7aacd188783b9ea
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-02-10 20:36:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (44.55%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int solve(int i,vector<int>& nums, vector<int>& dp){
-        if(i<0) return 0;
-        if(i==0) return nums[0];
-        if(dp[i]!=-1) return dp[i];
-        //here we are seeing if the summ till left adjacent is greater or the sum of current + till(i-2) is greater
-        return dp[i] = max(solve(i-1,nums,dp),solve(i-2,nums,dp)+nums[i]);
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
+        if(n==0) return 0;
         if(n==1) return nums[0];
-        vector<int> dp(n+1,-1);
-        return solve(n-1,nums,dp);
+        vector<int>dp(n);
+        dp[0]=nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
     }
 };
