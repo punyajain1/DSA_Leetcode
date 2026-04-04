d4c7acb21865c36b57a80d139dc856a5a4ddd0f3
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-04-04 09:48:16 +0530

    Time: 15 ms (90.04%) | Memory: 60.2 MB (91.53%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -6,9 +6,7 @@ public:
         sort(nums.begin(),nums.end());
         int ans=1 ,temp=1;
         for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]) {
-                continue;
-            }
+            if(nums[i]==nums[i-1]) continue;
             else if(nums[i]==nums[i-1]+1){
                 temp++;
             }else{
