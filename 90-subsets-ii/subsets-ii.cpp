16ce45960e90fdc289ba85e7237c31811f10f487
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-06-12 15:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (30.92%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    void sub(vector<vector<int>> &v, int i, vector<int> &ans, vector<int> &nums) {
+    vector<vector<int>> v;
+    void sub(int i, vector<int> &ans, vector<int> &nums) {
         v.push_back(ans);
+        
         for (int j = i; j < nums.size(); j++) {
-            if (j > i && nums[j] == nums[j - 1]) continue;
-            ans.push_back(nums[j]);
-            sub(v, j + 1, ans, nums);
-            ans.pop_back();
+            if (j > i && nums[j] == nums[j - 1]) continue; // skip duplicate
+            ans.push_back(nums[j]); //push current in ans
+            sub(j + 1, ans, nums); // moving to next recursion
+            ans.pop_back(); //poping the pushed above 
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> v;
         vector<int> ans;
         sort(nums.begin(), nums.end());
-        sub(v,0,ans,nums);
+        sub(0,ans,nums);
         return v;
 
     }
