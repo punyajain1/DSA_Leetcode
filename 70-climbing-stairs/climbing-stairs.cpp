077d94cf17c8be7f045fce94a7d610c90f905490
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-06-18 10:48:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.79%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    int climbStairs(int n){
-        vector<int> dp(n+1,-1);
-        dp[0]= 1;
-        dp[1]= 1;
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1]+ dp[i-2];
+    int climbStairs(int n) {
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        //as there are at minimum 1 way to reach 0 and 1 
+        //dp[i] here i respesent ith step
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]; //this is to be done as - Last move was 1 step(either at 1 step back) or Last move was 2 steps (either we are 2 step back)
         }
         return dp[n];
+
     }
 };
