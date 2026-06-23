e35e1880913038739ac023dc6b0b635b858a2260
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-06-23 11:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (88.13%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -4,17 +4,19 @@ public:
         int n = s.size();
         vector<bool> possible(n+1,false);
         possible[n] = true;
+
         for(int i= n-1;i>=0;i--){
             for(auto word:wordDict){
                 int x = word.size();
-                if( i + x <= n && (s.substr(i,x)== word)){
-                    possible[i] = possible[i+x];
+                if(i+x <= n && (s.substr(i,x) == word)){
+                    possible[i]=possible[i+x];
                 }
                 if(possible[i]){
                     break;
                 }
             }
         }
+
         return possible[0];
     }
 };
