5d907e7e99841015b15d210a40a6c8c215e2ad5b
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-06-24 13:14:01 +0530

    Time: 126 ms (59.29%) | Memory: 17.2 MB (70.20%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int s=0;
+        int n=nums.size(),t=0;
+        if(n==1) return false;
         for(int i=0;i<n;i++){
-            s+=nums[i];
+            t+=nums[i];
         }
-        if(s%2!=0) return false;
-        s=s/2;
-        vector<vector<bool>>dp(n+1,vector<bool>(s+1,false));
-        for (int i=0;i<=n;i++){
+        if(t%2!=0) return false;
+        //its prboblem is live if we can for the target which is equal to half of sum as we want to devide in 2 equal subset
+        int t2=t/2;
+        //dp knapsack problem either take it or leave it
+        vector<vector<bool>> dp(n+1, vector<bool>(t2+1, false));
+        for(int i=0;i<n;i++){
+            //as all were target is 0 we can for the subset
             dp[i][0]=true;
         }
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=s;j++){
-                dp[i][j] = dp[i-1][j];
-                if(j>=nums[i-1]) {
-                    dp[i][j] = dp[i][j] || dp[i-1][j - nums[i-1]];
+            for(int j=1;j<=t2;j++){
+                
+                //can we make j using nums[i-1](i starts from 1)
+                if(nums[i-1]<=j){
+                    dp[i][j] = ( dp[i-1][j] || dp[i-1][j-nums[i-1]] );
                 }
+                //if we cant
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }              
             }
         }
-        return dp[n][s];
+        //returning last element as the last will decide if its possible to make the partition of eqal subset sum 
+        return dp[n][t2];
     }
 };
