46e1cce494485dda7f31e75fe1433dfdf224abee
Punya Jain <161185181+punyajain1@users.noreply.github.com>
2026-07-16 15:40:31 +0530

    Time: 6 ms (38.00%) | Memory: 19.1 MB (19.92%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    int dfs(int i,vector<int>&v, vector<int>&p , vector<vector<int>>&adj ,vector<int>&ans){
-        if(p[i]==1) return 0;
-        if(v[i]==1) return 1;
-        p[i]=1;
-        v[i]=1;
-
-        for(auto it:adj[i]){
-            if(dfs(it,v,p,adj,ans)==0) return 0;
-        }
-        p[i]=0;
-        ans.push_back(i);
-        return 1;
-    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
-        for(auto it:prerequisites){
-            adj[it[0]].push_back(it[1]);
+        vector<vector<int>> adj(numCourses);
+        vector<int> ind(numCourses);
+        //using khans algo
+        for(auto pre : prerequisites){
+            adj[pre[1]].push_back(pre[0]);
         }
-        vector<int>v(numCourses,0);
-        vector<int>p(numCourses,0);
-        vector<int>ans;
         for(int i=0;i<numCourses;i++){
-            if(v[i]==0){
-                if(!dfs(i,v,p,adj,ans)) return {};
+            for(auto it:adj[i]){
+                ind[it]++;
             }
         }
+        vector<int> ans;
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(ind[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int t=q.front();
+            q.pop();
+            ans.push_back(t);
+            for(auto it:adj[t]){
+                ind[it]--;
+                if(ind[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(ans.size()!=numCourses) return {};
         return ans;
     }
 };
